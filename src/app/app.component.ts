4d8a840de136098bff746f22d7f09a3f48f205dc
Seyha Vorn <seyha.vorn@bronxtechnology.com>
2023-03-30 11:15:26 +0700

    init scanner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import {
+  ScannerQRCodeConfig,
+  ScannerQRCodeSelectedFiles,
+  NgxScannerQrcodeService,
+  ScannerQRCodeResult,
+  NgxScannerQrcodeComponent
+} from 'ngx-scanner-qrcode';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular-scanner';
+  public config: ScannerQRCodeConfig = {
+    // fps: 1000,
+    vibrate: 400,
+    // isBeep: true,
+    // decode: 'macintosh',
+    deviceActive: 0, // Camera 1 active
+    constraints: { 
+      audio: false,
+      video: {
+        width: window.innerWidth
+      }
+    } 
+  };
+
+  public qrCodeResult: ScannerQRCodeSelectedFiles[] = [];
+  public qrCodeResult2: ScannerQRCodeSelectedFiles[] = [];
+
+  @ViewChild('action', { static: true }) action!: NgxScannerQrcodeComponent;
+
+  constructor(private qrcode: NgxScannerQrcodeService) { }
+
+  ngAfterViewInit(): void {
+    this.action.isReady.pipe(delay(1000)).subscribe(() => {
+      this.action.start()
+    });
+  }
+
+  public onEvent(e: ScannerQRCodeResult[]): void {
+    console.log(e);
+  }
+
+  public handle(action: any, fn: string): void {
+    action[fn]().subscribe(console.log, alert);
+  }
+
+  public onDowload(action: any): void {
+    action.download().subscribe(console.log, alert);
+  }
+
+  public onSelects(files: any): void {
+    this.qrcode.loadFiles(files).subscribe((res: ScannerQRCodeSelectedFiles[]) => {
+      this.qrCodeResult = res;
+    });
+  }
+
+  public onSelects2(files: any): void {
+    this.qrcode.loadFilesToScan(files, this.config).subscribe((res: ScannerQRCodeSelectedFiles[]) => {
+      console.log(res);
+      this.qrCodeResult2 = res;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxScannerQrcodeModule } from 'ngx-scanner-qrcode';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SafePipe } from '../app/safe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxScannerQrcodeModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
